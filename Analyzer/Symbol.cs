146031b10484e3c706e4d2032391dd9d8f7d74d2
SamuelHenrique32 <shdalmas@ucs.br>
2020-05-15 22:12:24 -0300

    symbolsTable

---
+++ Analyzer/Symbol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Analyzer
+{
+	public class Symbol
+	{
+		public string symbol { get; set; }
+
+		public List<int> lines = new List<int>();
+
+		public List<int> columns = new List<int>();
+
+	}
+}
+
