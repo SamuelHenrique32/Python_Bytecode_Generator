6d46a97c6acaca2bbb7cd7d9d55a45fa49fb80f1
SamuelHenrique32 <shdalmas@ucs.br>
2020-05-17 17:53:19 -0300

    Simple assignment

---
+++ Analyzer/BytecodeRegister.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyzer
+{
+    public class BytecodeRegister
+    {
+        public int lineInGeneratedBytecode { get; set; }
+
+        public int lineInFile { get; set; }
+
+        public int offset { get; set; }
+
+        public int opCode { get; set; }
+
+        public int stackPos { get; set; }
+
+        public String preview { get; set; }
+    }
+}
